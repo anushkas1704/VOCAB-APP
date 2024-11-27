30f984ba1883af962a450d3e05d3f9c094c1bcdf
anushkas1704 <anushka.600818@gmail.com>
2024-11-27 11:56:22 +0530

    css changed

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send("Hello from the Vocabulary App Backend!");
-});
-
-app.listen(3001, () => console.log('Backend running on port 3001'));
